7ed54a33a453aafeb85a845aa39bb8403c40e7e8
Thato-Maboya <52561664+Thato-Maboya@users.noreply.github.com>
2021-01-15 08:02:40 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,12 +6,12 @@ function Slider() {
 
 function goToResearch(){
     console.log("click works");
-    document.location = "/bitswebsite/research_info.html";
+    document.location = "/research_info.html";
 }
 
 function goToService(){
     console.log("click works");
-    document.location = "/bitswebsite/service_info.html";
+    document.location = "/service_info.html";
 }
 
 
